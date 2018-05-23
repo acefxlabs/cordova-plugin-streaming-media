71294af50befd72d7db24c1702c570810eb59113
Oyewumi Abayomi <ace@acefxlab.com>
2018-05-23 01:46:34 +0100

    Make Player Underground

--- src/android/com/hutchind/cordova/plugins/streamingmedia/SimpleAudioStream.java
+++ src/android/com/hutchind/cordova/plugins/streamingmedia/SimpleAudioStream.java
@@ -23,264 +23,241 @@ public class SimpleAudioStream extends Activity implements
 		MediaController.MediaPlayerControl {
 
 	private String TAG = getClass().getSimpleName();
-	private MediaPlayer mMediaPlayer = null;
-	private MediaController mMediaController = null;
-	private LinearLayout mAudioView;
-	private View mMediaControllerView;
-	private String mAudioUrl;
-	private Boolean mShouldAutoClose = true;
+    private MediaPlayer mMediaPlayer = null;
+    private View mMediaControllerView;
+    private String mAudioUrl;
+    private Boolean mShouldAutoClose = true;
 
-	@Override
-	public void onCreate(Bundle icicle) {
-		super.onCreate(icicle);
-		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-		Bundle b = getIntent().getExtras();
-		mAudioUrl = b.getString("mediaUrl");
-		String backgroundColor = b.getString("bgColor");
-		String backgroundImagePath = b.getString("bgImage");
-		String backgroundImageScale = b.getString("bgImageScale");
-		mShouldAutoClose = b.getBoolean("shouldAutoClose");
-		mShouldAutoClose = mShouldAutoClose == null ? true : mShouldAutoClose;
-		backgroundImageScale = backgroundImageScale == null ? "center" : backgroundImageScale.toLowerCase();
-		ImageView.ScaleType bgImageScaleType;
-		// Default background to black
-		int bgColor = Color.BLACK;
-		if (backgroundColor != null) {
-			bgColor = Color.parseColor(backgroundColor);
-		}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle b = getIntent().getExtras();
+        mAudioUrl = b.getString("mediaUrl");
+        String backgroundColor = b.getString("bgColor");
+        String backgroundImagePath = b.getString("bgImage");
+        String backgroundImageScale = b.getString("bgImageScale");
+        mShouldAutoClose = b.getBoolean("shouldAutoClose");
+        mShouldAutoClose = mShouldAutoClose == null ? true : mShouldAutoClose;
+        backgroundImageScale = backgroundImageScale == null ? "center" : backgroundImageScale.toLowerCase();
+        ImageView.ScaleType bgImageScaleType;
+        // Default background to black
+        /*int bgColor = Color.BLACK;
+        if (backgroundColor != null) {
+            bgColor = Color.parseColor(backgroundColor);
+        }
 
-		if (backgroundImageScale.equals("fit")) {
-			bgImageScaleType = ImageView.ScaleType.FIT_CENTER;
-		} else if (backgroundImageScale.equals("stretch")) {
-			bgImageScaleType = ImageView.ScaleType.FIT_XY;
-		} else {
-			bgImageScaleType = ImageView.ScaleType.CENTER;
-		}
+        if (backgroundImageScale.equals("fit")) {
+            bgImageScaleType = ImageView.ScaleType.FIT_CENTER;
+        } else if (backgroundImageScale.equals("stretch")) {
+            bgImageScaleType = ImageView.ScaleType.FIT_XY;
+        } else {
+            bgImageScaleType = ImageView.ScaleType.CENTER;
+        }*/
 
-		RelativeLayout audioView = new RelativeLayout(this);
-		audioView.setBackgroundColor(bgColor);
+        RelativeLayout audioView = new RelativeLayout(this);
+        //audioView.setBackgroundColor(bgColor);
 
-		if (backgroundImagePath != null) {
-			ImageView bgImage = new ImageView(this);
-			new ImageLoadTask(backgroundImagePath, bgImage, getApplicationContext()).execute(null, null);
-			RelativeLayout.LayoutParams bgImageLayoutParam = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
-			bgImageLayoutParam.addRule(RelativeLayout.CENTER_IN_PARENT);
-			bgImage.setLayoutParams(bgImageLayoutParam);
-			bgImage.setScaleType(bgImageScaleType);
-			audioView.addView(bgImage);
-		}
+        /*if (backgroundImagePath != null) {
+            ImageView bgImage = new ImageView(this);
+            new ImageLoadTask(backgroundImagePath, bgImage, getApplicationContext()).execute(null, null);
+            RelativeLayout.LayoutParams bgImageLayoutParam = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+            bgImageLayoutParam.addRule(RelativeLayout.CENTER_IN_PARENT);
+            bgImage.setLayoutParams(bgImageLayoutParam);
+            bgImage.setScaleType(bgImageScaleType);
+            audioView.addView(bgImage);
+        }*/
 
-		RelativeLayout.LayoutParams relLayoutParam = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
-		mMediaControllerView = new View(this);
-		audioView.addView(mMediaControllerView);
-		setContentView(audioView, relLayoutParam);
+        RelativeLayout.LayoutParams relLayoutParam = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+        mMediaControllerView = new View(this);
+        audioView.addView(mMediaControllerView);
+        setContentView(audioView, relLayoutParam);
+		
+		//this line ensures transparent window does not respond to touch
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
 
-		play();
-	}
+        play();
+    }
 
-	private void play() {
-		Uri myUri = Uri.parse(mAudioUrl);
-		try {
-			if (mMediaPlayer == null) {
-				mMediaPlayer = new MediaPlayer();
-				//mMediaPlayer = MediaPlayer.create(this.uri);
-			} else {
-				try {
-					mMediaPlayer.stop();
-					mMediaPlayer.reset();
-				} catch (Exception e) {
-					Log.e(TAG, e.toString());
-				}
-			}
-			mMediaPlayer.setDataSource(this, myUri); // Go to Initialized state
-			mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-			mMediaPlayer.setOnPreparedListener(this);
-			mMediaPlayer.setOnCompletionListener(this);
-			mMediaPlayer.setOnBufferingUpdateListener(this);
-			mMediaPlayer.setOnErrorListener(this);
-			mMediaController = new MediaController(this);
+    private void play() {
+        try {
+            if (mMediaPlayer == null) {
+                mMediaPlayer = new MediaPlayer();
+            } else {
+                try {
+                    mMediaPlayer.stop();
+                    mMediaPlayer.reset();
+                } catch (Exception e) {
+                    Log.e(TAG, e.toString());
+                }
+            }
+            mMediaPlayer.setDataSource(mAudioUrl); // Go to Initialized state
+            mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            mMediaPlayer.setOnPreparedListener(this);
+            mMediaPlayer.setOnCompletionListener(this);
+            mMediaPlayer.setOnBufferingUpdateListener(this);
+            mMediaPlayer.setOnErrorListener(this);
 
-			mMediaPlayer.prepareAsync();
-			
-			//mMediaPlayer.hide();
+            mMediaPlayer.prepareAsync();
 
-			Log.d(TAG, "LoadClip Done");
-		} catch (Throwable t) {
-			Log.d(TAG, t.toString());
-		}
-	}
+            Log.d(TAG, "LoadClip Done");
+        } catch (Exception t) {
+            Log.d(TAG, t.getMessage());
+        }
+    }
 
-	@Override
-	public void onPrepared(MediaPlayer mp) {
-		Log.d(TAG, "Stream is prepared");
-		mMediaController.setMediaPlayer(this);
-		mMediaController.setAnchorView(mMediaControllerView);
-		mMediaPlayer.start();
-		mMediaController.setEnabled(true);
-		mMediaController.show();
-	}
+    private void stop() {
+        if (mMediaPlayer!=null) {
+            try {
+                mMediaPlayer.stop();
+            } catch (Exception e) {
+                Log.d(TAG, e.toString());
+            }
+        }
+    }
 
-	@Override
-	public void start() {
-		if (mMediaPlayer!=null) {
-			mMediaPlayer.start();
-		}
-	}
+    private void wrapItUp(int resultCode, String message) {
+        Intent intent = new Intent();
+        intent.putExtra("message", message);
+        setResult(resultCode, intent);
+        finish();
+    }
 
-	@Override
-	public void pause() {
-		if (mMediaPlayer!=null) {
-			try {
-				mMediaPlayer.pause();
-			} catch (Exception e) {
-				Log.d(TAG, e.toString());
-			}
-		}
-	}
+    public void onBackPressed() {
+        wrapItUp(RESULT_OK, null);
+    }
 
-	private void stop() {
-		if (mMediaPlayer!=null) {
-			try {
-				mMediaPlayer.stop();
-			} catch (Exception e) {
-				Log.d(TAG, e.toString());
-			}
-		}
-	}
+    @Override
+    public void onBufferingUpdate(MediaPlayer mp, int percent) {
+        Log.d(TAG, "PlayerService onBufferingUpdate : " + percent + "%");
+    }
 
-	public int getDuration() {
-		return (mMediaPlayer!=null) ? mMediaPlayer.getDuration() : 0;
-	}
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        stop();
+        if (mShouldAutoClose) {
+            Log.v(TAG, "FINISHING ACTIVITY");
+            wrapItUp(RESULT_OK, null);
+        }
+    }
 
-	public int getCurrentPosition() {
-		return (mMediaPlayer!=null) ? mMediaPlayer.getCurrentPosition() : 0;
-	}
+    @Override
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Media Player Error: ");
+        switch (what) {
+            case MediaPlayer.MEDIA_ERROR_NOT_VALID_FOR_PROGRESSIVE_PLAYBACK:
+                sb.append("Not Valid for Progressive Playback");
+                break;
+            case MediaPlayer.MEDIA_ERROR_SERVER_DIED:
+                sb.append("Server Died");
+                break;
+            case MediaPlayer.MEDIA_ERROR_UNKNOWN:
+                sb.append("Unknown");
+                break;
+            default:
+                sb.append(" Non standard (");
+                sb.append(what);
+                sb.append(")");
+        }
+        sb.append(" (" + what + ") ");
+        sb.append(extra);
+        Log.e(TAG, sb.toString());
+        wrapItUp(RESULT_CANCELED, sb.toString());
+        return true;
+    }
 
-	public void seekTo(int i) {
-		if (mMediaPlayer!=null) {
-			mMediaPlayer.seekTo(i);
-		}
-	}
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        Log.d(TAG, "Stream is prepared");
+        mMediaPlayer.start();
+    }
 
-	public boolean isPlaying() {
-		if (mMediaPlayer!=null) {
-			try {
-				return mMediaPlayer.isPlaying();
-			} catch (Exception e) {
-				Log.d(TAG, e.toString());
-			}
-		}
-		return false;
-	}
+    @Override
+    public void start() {
+        if (mMediaPlayer!=null) {
+            mMediaPlayer.start();
+        }
+    }
 
-	public int getBufferPercentage() {
-		return 0;
-	}
+    @Override
+    public void pause() {
+        if (mMediaPlayer!=null) {
+            try {
+                mMediaPlayer.pause();
+            } catch (Exception e) {
+                Log.d(TAG, e.toString());
+            }
+        }
+    }
 
-	public boolean canPause() {
-		return true;
-	}
+    @Override
+    public int getDuration() {
+        return (mMediaPlayer!=null) ? mMediaPlayer.getDuration() : 0;
+    }
 
-	public boolean canSeekBackward() {
-		return true;
-	}
+    @Override
+    public int getCurrentPosition() {
+        return (mMediaPlayer!=null) ? mMediaPlayer.getCurrentPosition() : 0;
+    }
 
-	public boolean canSeekForward() {
-		return true;
-	}
+    @Override
+    public void seekTo(int pos) {
+        if (mMediaPlayer!=null) {
+            mMediaPlayer.seekTo(pos);
+        }
+    }
 
-	@Override
-	public int getAudioSessionId() {
-		return 0;
-	}
+    @Override
+    public boolean isPlaying() {
+        if (mMediaPlayer!=null) {
+            try {
+                return mMediaPlayer.isPlaying();
+            } catch (Exception e) {
+                Log.d(TAG, e.toString());
+            }
+        }
+        return false;
+    }
 
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		if (mMediaPlayer!=null){
-			try {
-				mMediaPlayer.reset();
-				mMediaPlayer.release();
-			} catch (Exception e) {
-				Log.e(TAG, e.toString());
-			}
-			mMediaPlayer = null;
-		}
-	}
+    @Override
+    public int getBufferPercentage() {
+        return 0;
+    }
 
-	private void wrapItUp(int resultCode, String message) {
-		Intent intent = new Intent();
-		intent.putExtra("message", message);
-		setResult(resultCode, intent);
-		finish();
-	}
+    @Override
+    public boolean canPause() {
+        return true;
+    }
 
+    @Override
+    public boolean canSeekBackward() {
+        return true;
+    }
 
-	@Override
-	public void onCompletion(MediaPlayer mp) {
-		stop();
-		if (mShouldAutoClose) {
-			Log.v(TAG, "FINISHING ACTIVITY");
-			wrapItUp(RESULT_OK, null);
-		}
+    @Override
+    public boolean canSeekForward() {
+        return true;
+    }
 
-	}
+    @Override
+    public int getAudioSessionId() {
+        return 0;
+    }
 
-	public boolean onError(MediaPlayer mp, int what, int extra) {
-		StringBuilder sb = new StringBuilder();
-		sb.append("Media Player Error: ");
-		switch (what) {
-			case MediaPlayer.MEDIA_ERROR_NOT_VALID_FOR_PROGRESSIVE_PLAYBACK:
-				sb.append("Not Valid for Progressive Playback");
-				break;
-			case MediaPlayer.MEDIA_ERROR_SERVER_DIED:
-				sb.append("Server Died");
-				break;
-			case MediaPlayer.MEDIA_ERROR_UNKNOWN:
-				sb.append("Unknown");
-				break;
-			default:
-				sb.append(" Non standard (");
-				sb.append(what);
-				sb.append(")");
-		}
-		sb.append(" (" + what + ") ");
-		sb.append(extra);
-		Log.e(TAG, sb.toString());
-		wrapItUp(RESULT_CANCELED, sb.toString());
-		return true;
-	}
+    public void onDestroy() {
+        super.onDestroy();
+        if (mMediaPlayer!=null){
+            try {
+                mMediaPlayer.reset();
+                mMediaPlayer.release();
+            } catch (Exception e) {
+                Log.e(TAG, e.toString());
+            }
+            mMediaPlayer = null;
+        }
+    }
 
-	public void onBufferingUpdate(MediaPlayer mp, int percent) {
-		Log.d(TAG, "PlayerService onBufferingUpdate : " + percent + "%");
-	}
-
-	@Override
-	public void onBackPressed() {
-		wrapItUp(RESULT_OK, null);
-	}
-
-	@Override
-	public void onConfigurationChanged(Configuration newConfig) {
-		super.onConfigurationChanged(newConfig);
-	}
-
-	@Override
-	protected void onStop() {
-		super.onStop();
-		mMediaController.hide();
-		try {
-			mMediaPlayer.stop();
-			mMediaPlayer.reset();
-			mMediaPlayer.release();
-		} catch(Exception e) {
-			Log.e(TAG, e.toString());
-		}
-	}
-
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		mMediaController.show();
-		return false;
-	}
+    public boolean onTouchEvent(MotionEvent event) {
+        return false;
+    }
 }
