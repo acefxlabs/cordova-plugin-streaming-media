10515ee4b7dc28f4d9648f8bbb7d1a868c7335a9
Oyewumi Abayomi <ace@acefxlab.com>
2018-05-23 18:20:03 +0100

    Update SimpleAudioStream.java

--- src/android/com/hutchind/cordova/plugins/streamingmedia/SimpleAudioStream.java
+++ src/android/com/hutchind/cordova/plugins/streamingmedia/SimpleAudioStream.java
@@ -1,55 +1,45 @@
 package com.hutchind.cordova.plugins.streamingmedia;
 
-import android.app.Service;
+import android.app.Activity;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.MediaController;
 
-import java.io.IOException;
+public class SimpleAudioStream extends Activity implements
+        MediaPlayer.OnCompletionListener, MediaPlayer.OnPreparedListener,
+        MediaPlayer.OnErrorListener, MediaPlayer.OnBufferingUpdateListener,
+        MediaController.MediaPlayerControl {
 
-import static android.app.Activity.RESULT_CANCELED;
-import static android.app.Activity.RESULT_OK;
-
-public class SimpleAudioStream extends Service implements MediaPlayer.OnPreparedListener,
-        MediaPlayer.OnErrorListener, MediaPlayer.OnCompletionListener, MediaPlayer.OnBufferingUpdateListener {
-
-    private MediaPlayer mMediaPlayer;
     private String TAG = getClass().getSimpleName();
-
-    public SimpleAudioStream() {
-    }
+    private MediaPlayer mMediaPlayer = null;
 
     @Override
-    public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-            Log.d(TAG, "Starting player command...");
-        //Bundle bundle = intent.getExtras();
-        //if (bundle != null) {
-            String mediaUrl = "http://radio.domi.org.ng:8000/domi_media";// bundle.getString("mediaUrl");
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_audio_stream_x);
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            Log.d(TAG, "Starting command...");
+            String mediaUrl = bundle.getString("mediaUrl");
             play(mediaUrl);
-        //}
-        return super.onStartCommand(intent, flags, startId);
+        }
+        //getWindow().addFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
     }
 
     private void play(String mAudioUrl) {
-            Log.d(TAG, "Play called...");
         if (mMediaPlayer == null) {
-                Log.d(TAG, "Player initialized...");
+            Log.d(TAG, "Creating player...");
             mMediaPlayer = new MediaPlayer();
         } else {
             mMediaPlayer.stop();
             mMediaPlayer.reset();
         }
         try {
-                Log.d(TAG, "Set player datasource...");
+            Log.d(TAG, "Streaming service datasource..." + mAudioUrl);
             mMediaPlayer.setDataSource(mAudioUrl); // Go to Initialized state
             mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
             mMediaPlayer.setOnPreparedListener(this);
@@ -65,9 +55,41 @@ public class SimpleAudioStream extends Service implements MediaPlayer.OnPrepared
         }
     }
 
+    private void stop() {
+        if (mMediaPlayer != null) {
+            try {
+                mMediaPlayer.stop();
+            } catch (Exception e) {
+                Log.d(TAG, e.toString());
+            }
+        }
+    }
+
+    private void wrapItUp(int resultCode, String message) {
+        Intent intent = new Intent();
+        intent.putExtra("message", message);
+        setResult(resultCode, intent);
+        finish();
+    }
+
+    public void onBackPressed() {
+        wrapItUp(RESULT_OK, null);
+    }
+
+    @Override
+    public void onBufferingUpdate(MediaPlayer mp, int percent) {
+        Log.d(TAG, "PlayerService onBufferingUpdate : " + percent + "%");
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        stop();
+        Log.v(TAG, "FINISHING ACTIVITY");
+        wrapItUp(RESULT_OK, null);
+    }
+
     @Override
     public boolean onError(MediaPlayer mp, int what, int extra) {
-        mMediaPlayer.reset();
         StringBuilder sb = new StringBuilder();
         sb.append("Media Player Error: ");
         switch (what) {
@@ -98,20 +120,42 @@ public class SimpleAudioStream extends Service implements MediaPlayer.OnPrepared
         mMediaPlayer.start();
     }
 
+    @Override
+    public void start() {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.start();
+        }
+    }
+
+    @Override
+    public void pause() {
+        if (mMediaPlayer != null) {
+            try {
+                mMediaPlayer.pause();
+            } catch (Exception e) {
+                Log.d(TAG, e.toString());
+            }
+        }
+    }
+
+    @Override
     public int getDuration() {
         return (mMediaPlayer != null) ? mMediaPlayer.getDuration() : 0;
     }
 
+    @Override
     public int getCurrentPosition() {
         return (mMediaPlayer != null) ? mMediaPlayer.getCurrentPosition() : 0;
     }
 
+    @Override
     public void seekTo(int pos) {
         if (mMediaPlayer != null) {
             mMediaPlayer.seekTo(pos);
         }
     }
 
+    @Override
     public boolean isPlaying() {
         if (mMediaPlayer != null) {
             try {
@@ -123,22 +167,27 @@ public class SimpleAudioStream extends Service implements MediaPlayer.OnPrepared
         return false;
     }
 
+    @Override
     public int getBufferPercentage() {
         return 0;
     }
 
+    @Override
     public boolean canPause() {
         return true;
     }
 
+    @Override
     public boolean canSeekBackward() {
         return true;
     }
 
+    @Override
     public boolean canSeekForward() {
         return true;
     }
 
+    @Override
     public int getAudioSessionId() {
         return 0;
     }
@@ -156,38 +205,7 @@ public class SimpleAudioStream extends Service implements MediaPlayer.OnPrepared
         }
     }
 
-    private void stop() {
-        if (mMediaPlayer != null) {
-            try {
-                mMediaPlayer.stop();
-            } catch (Exception e) {
-                Log.d(TAG, e.toString());
-            }
-        }
-    }
-
-    private void wrapItUp(int resultCode, String message) {
-        Intent intent = new Intent();
-        intent.putExtra("message", message);
-        //setResult(resultCode, intent);
-        //finish();
-    }
-
-    public void onBackPressed() {
-        wrapItUp(RESULT_OK, null);
-    }
-
-    @Override
-    public void onBufferingUpdate(MediaPlayer mp, int percent) {
-        Log.d(TAG, "PlayerService onBufferingUpdate : " + percent + "%");
-    }
-
-    @Override
-    public void onCompletion(MediaPlayer mp) {
-        stop();
-        //if (mShouldAutoClose) {
-        Log.v(TAG, "FINISHING ACTIVITY");
-        wrapItUp(RESULT_OK, null);
-        //}
+    public boolean onTouchEvent(MotionEvent event) {
+        return false;
     }
 }
