a73b85b0147f13a54b12bb2da2a9c114de05c08a
Oyewumi Abayomi <ace@acefxlab.com>
2018-05-23 16:16:14 +0100

    Update SimpleAudioStream.java

--- src/android/com/hutchind/cordova/plugins/streamingmedia/SimpleAudioStream.java
+++ src/android/com/hutchind/cordova/plugins/streamingmedia/SimpleAudioStream.java
@@ -30,6 +30,7 @@ public class SimpleAudioStream extends Service implements MediaPlayer.OnPrepared
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+            Log.d(TAG, "Starting player command...");
         //Bundle bundle = intent.getExtras();
         //if (bundle != null) {
             String mediaUrl = "http://radio.domi.org.ng:8000/domi_media";// bundle.getString("mediaUrl");
@@ -39,13 +40,16 @@ public class SimpleAudioStream extends Service implements MediaPlayer.OnPrepared
     }
 
     private void play(String mAudioUrl) {
+            Log.d(TAG, "Play called...");
         if (mMediaPlayer == null) {
+                Log.d(TAG, "Player initialized...");
             mMediaPlayer = new MediaPlayer();
         } else {
             mMediaPlayer.stop();
             mMediaPlayer.reset();
         }
         try {
+                Log.d(TAG, "Set player datasource...");
             mMediaPlayer.setDataSource(mAudioUrl); // Go to Initialized state
             mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
             mMediaPlayer.setOnPreparedListener(this);
